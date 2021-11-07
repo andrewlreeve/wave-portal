4e54d9eb95d742302fc11e5902ce81ca4829ffdc
Andrew Reeve <andrew@andrewlreeve.com>
2021-11-08 08:56:25 +1300

    include contract endpoints

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,31 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint256 totalWaves;
+  uint256 totalShakas;
+
   constructor() {
     console.log("Yo yo, I'm a smartish contract");
+  }
+
+  function wave() public {
+    totalWaves += 1;
+    console.log("%s waved!", msg.sender);
+  }
+
+  function shaka() public {
+    totalWaves += 1;
+    totalShakas += 1;
+    console.log("%s shaka bruh!", msg.sender);
+  }
+
+  function getTotalWaves() public view returns (uint256) {
+    console.log("We have %d total waves!", totalWaves);
+    return totalWaves;
+  }
+
+  function getTotalShakas() public view returns (uint256) {
+    console.log("We have %d total shakas!", totalShakas);
+    return totalShakas;
   }
 }
--- scripts/run.ts
+++ scripts/run.ts
@@ -1,13 +1,35 @@
+// Typescript requires the explicit import of HRE
 import { ethers } from "hardhat";
 
 async function main() {
+  const [owner, randomPerson] = await ethers.getSigners();
   // Compiles the contract
   const waveContractFactory = await ethers.getContractFactory("WavePortal");
   // Deploys the contract
   const waveContract = await waveContractFactory.deploy();
   // Waits for the contract to be deployed
   await waveContract.deployed();
+
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  let shakaCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  let shakaTxn = await waveContract.connect(randomPerson).shaka();
+  waveCount = await waveContract.getTotalWaves();
+  shakaCount = await waveContract.getTotalShakas();
 }
 
 async function runMain() {
